e3b1441ecea73f1bcbb451cc623a54bedf1e8c78
Dimand Márton <dimand.marton@gmail.com>
2021-09-27 16:57:01 +0200

    Teljesen kész

--- Drill3/Drill3.cpp
+++ Drill3/Drill3.cpp
@@ -2,27 +2,39 @@
 
 int main()
 {
-	cout << "Enter the name of the person you want to write to:\n";
-	string first_name;
-	cin >> first_name;
-	cout << "Dear, " << first_name <<"\n\n"<< "How are you? I am fine. I miss you.\n\nPlease tell me how are you doing. How was your summer holiday?\n";
-	cout << "Please enter the name of another friend of yours:\n";
-	string friend_name;
-	cin >> friend_name;
-	cout << "Have you seen " << friend_name << " lately?\n";
+	string first_name = "", friend_name = "", letter = "";
 	char friend_sex = 0;
+	int age = 0;
+
+	cout << "Enter the name of the person you want to write to:\n";
+	cin >> first_name;
+	letter = "Dear " + first_name + ",\n\n\tHow are you? I am fine. I miss you.\n";
+	letter += "Please tell me how are you doing. How was your summer holiday?\n";
+	
+	cout << "Please enter the name of another friend of yours:\n";
+	cin >> friend_name;
+	letter += "Have you seen " + friend_name + " lately? ";
+	
 	cout << "What is your friend's gender? (male='m', female='f')\n";
 	cin >> friend_sex;
-	if (friend_sex=='m')
-		cout << "If you see " << friend_name << " please ask him to call me\n";
-	else cout << "If you see " << friend_name << " please ask her to call me\n";
-	cout << "What is the age of your friend?\n";
-	int age;
+	if (friend_sex == 'm')
+		letter += "If you see " + friend_name + " please ask him to call me.\n";
+	else 
+		letter += "If you see " + friend_name + " please ask her to call me.\n";
+	
+	cout << "What is the age of your recipient?\n";
 	cin >> age;
-	if (age < 12) 
-		cout << "Next year you will be" << age + 1 << "\n";
-	else if (age > 70) cout << "I hope you are enjoying retirement\n";
-	else if (age==17) cout << "Next year you will be able to vote\n";
-	cout << "Yours sincerely,\n\n\nDimand Márton\n";
+	if (age <= 0 || age >= 110)
+		simple_error("you're kidding!");
+	letter += "I hear you just had a birthday and you are " + to_string(age) + " years old.";
+	if (age < 12)
+		letter += "\nNext year you will be " + to_string(age + 1) + ".";
+	else if (age > 70) 
+		letter += "\nI hope you are enjoying retirement.";
+	else if (age == 17) 
+		letter += "\nNext year you will be able to vote.";
+
+	letter += "\n\nYours sincerely,\n\n\nDimand Márton\n";
+	cout << "Here is your whole letter:\n\n" << letter;
 	return 0;
 }
